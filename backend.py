db8e1edea746f847d5af95769e5e08cba678eadc
SanjitGanesh <152583247+SanjitGanesh@users.noreply.github.com>
2026-07-15 09:03:55 +0530

    added the filtering system for the data

--- backend.py
+++ backend.py
@@ -1,4 +1,18 @@
+import requests
+
 API_KEY = "6d4d383df9be86666fd20a85a007d985"
-def get_data(place,forecast_days,kind):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}"
-    return url
+def get_data(place,forecast_days=None,kind=None):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data['list']
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    elif kind == "sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo",forecast_days=2,kind='temperature'))
