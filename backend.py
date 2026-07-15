e952f30b4d3f9eb8bae883ab5b46cb7e510f6cae
SanjitGanesh <152583247+SanjitGanesh@users.noreply.github.com>
2026-07-15 13:17:50 +0530

    before polishing

--- backend.py
+++ backend.py
@@ -1,18 +1,14 @@
 import requests
 
 API_KEY = "6d4d383df9be86666fd20a85a007d985"
-def get_data(place,forecast_days=None,kind=None):
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
-    response = requests.get(url)
-    data = response.json()
-    filtered_data = data['list']
-    nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    if kind == "temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    elif kind == "sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-    return filtered_data
+def get_data(place,forecast_days=None):
+        url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+        response = requests.get(url)
+        data = response.json()
+        filtered_data = data["list"]
+        nr_values = 8 * forecast_days
+        filtered_data = filtered_data[:nr_values]   
+        return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo",forecast_days=2,kind='temperature'))
+    get_data(place="Tokyo",forecast_days=2)
--- main.py
+++ main.py
@@ -7,11 +7,30 @@ place = st.text_input("place: ")
 days = st.slider("days",min_value=1, max_value=5,help="Select the number of days for the weather forecast")
 option = st.selectbox("Select the data to view",("Temperature","Sky"))
 
+
 st.subheader(f"{option} of {place} for the next {days} days")
 
 
+if place:
+    try:
+        filtered_data = get_data(place,days)
+        if isinstance(filtered_data,list):
+            if option == 'Temperature':
+                dates = [item['dt_txt'] for item in filtered_data]
+                temperatures = [dict["main"]["temp"]/10 for dict in filtered_data]
+                print(filtered_data)
+                
+                figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (°C)"}, title=f"Temperature Forecast for {place}")
+                st.plotly_chart(figure)
+            if option == 'Sky':
+                images = {"Clear":"images/clear.png","Clouds":"images/cloud.png",
+                        "Rain":"images/rain.png","Snow":"images/snow.png"}
 
-dates, temperatures = get_data(days)
-figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (°C)"}, title=f"Temperature Forecast for {place}")
+                sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+                image_paths = [images[condition] for condition in sky_conditions]
+                st.image(image_paths,width=115)
 
-st.plotly_chart(figure)
+    except KeyError:
+        st.error('Enter a valid city to get the info')
+    
+        
